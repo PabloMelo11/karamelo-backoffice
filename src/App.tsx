a04601ccaca5f3c51f0056149f5700d843207f6c
Pablo Melo <pablomelodev@gmail.com>
2020-08-12 02:54:54 -0300

    style: add global styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
 import React from 'react';
 
-function App() {
-  return <h1>Hello World</h1>;
-}
+import GlobalStyle from './styles/global';
+
+const App: React.FC = () => (
+  <>
+    <div>Hello World</div>
+    <GlobalStyle />
+  </>
+);
 
 export default App;
