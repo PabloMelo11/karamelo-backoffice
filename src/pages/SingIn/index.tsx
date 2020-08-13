90ba24d2364f3d487fb5b749e69aa9775b69048a
Pablo Melo <pablomelodev@gmail.com>
2020-08-12 21:40:53 -0300

    feat: configuring unform lib

--- src/pages/SingIn/index.tsx
+++ src/pages/SingIn/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
-import { FiLogIn, FiMail, FiLock } from 'react-icons/fi';
+import { Form } from '@unform/web';
+import { FiMail, FiLock } from 'react-icons/fi';
 
 import logoImg from '../../assets/logo.svg';
 
@@ -8,34 +9,35 @@ import Button from '../../components/Button';
 
 import { Container, Content, Background } from './styles';
 
-const SingIn: React.FC = () => (
-  <Container>
-    <Content>
-      <img src={logoImg} alt="Karamelo" />
+const SingIn: React.FC = () => {
+  function handleSubmit(data: object): void {
+    console.log(data);
+  }
 
-      <form>
-        <h1>Faça seu logon</h1>
+  return (
+    <Container>
+      <Content>
+        <img src={logoImg} alt="Karamelo" />
 
-        <Input name="email" icon={FiMail} placeholder="E-mail" />
-        <Input
-          name="password"
-          icon={FiLock}
-          type="password"
-          placeholder="Senha"
-        />
+        <Form onSubmit={handleSubmit}>
+          <h1>Faça seu logon</h1>
 
-        <Button type="submit">Entrar</Button>
+          <Input name="email" icon={FiMail} placeholder="E-mail" />
+          <Input
+            name="password"
+            icon={FiLock}
+            type="password"
+            placeholder="Senha"
+          />
 
-        <a href="forgot">Esqueci minha senha</a>
-      </form>
+          <Button type="submit">Entrar</Button>
 
-      <a href="logon">
-        <FiLogIn />
-        Criar conta
-      </a>
-    </Content>
-    <Background />
-  </Container>
-);
+          <a href="forgot">Esqueci minha senha</a>
+        </Form>
+      </Content>
+      <Background />
+    </Container>
+  );
+};
 
 export default SingIn;
