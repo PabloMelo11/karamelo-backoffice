df2df69b7a91a018c334243100570a932d655eeb
Pablo Melo <pablomelodev@gmail.com>
2020-08-13 01:12:56 -0300

    feat: validating registration

--- src/pages/SingIn/index.tsx
+++ src/pages/SingIn/index.tsx
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, { useCallback } from 'react';
 import { Form } from '@unform/web';
 import { FiMail, FiLock } from 'react-icons/fi';
+import * as Yup from 'yup';
 
 import logoImg from '../../assets/logo.svg';
 
@@ -10,9 +11,20 @@ import Button from '../../components/Button';
 import { Container, Content, Background } from './styles';
 
 const SingIn: React.FC = () => {
-  function handleSubmit(data: object): void {
-    console.log(data);
-  }
+  const handleSubmit = useCallback(async (data: object) => {
+    try {
+      const schema = Yup.object().shape({
+        email: Yup.string()
+          .required('E-mail obrigatório')
+          .email('E-mail inválido'),
+        password: Yup.string().required('Senha obrigatória'),
+      });
+
+      await schema.validate(data, { abortEarly: false });
+    } catch (err) {
+      console.log(err);
+    }
+  }, []);
 
   return (
     <Container>
